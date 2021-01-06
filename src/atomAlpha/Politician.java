01b4eaa045cf8bd7db0d55163c1c74a7793cbe38
Pie Graph <viswarath123@gmail.com>
2021-01-06 11:59:36 -0500

    Tracking

--- src/atomAlpha/Politician.java
+++ src/atomAlpha/Politician.java
@@ -6,16 +6,46 @@ public class Politician {
     public static String role = "";
 
     public static void run(RobotController rc, int turnCount) throws GameActionException {
+
         Team enemy = rc.getTeam().opponent();
-        int actionRadius = rc.getType().actionRadiusSquared;
-        RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
-        if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-            // System.out.println("empowering...");
-            rc.empower(actionRadius);
-            // System.out.println("empowered");
-            return;
-        }
+
+        MapLocation myLoc = rc.getLocation();
+
+        int detectionRadiusSquared = 25;
+        int actionRadiusSquared = 9;
+        int chaseCount = 0;
+
+        RobotInfo[] attackable = rc.senseNearbyRobots(myLoc, detectionRadiusSquared, enemy);
+
         if (role.length() == 7) {
+
+        // create a locking mechanism and chasing mechanism
+        //marks the last known location of the closest enemy bot, no flags needed
+        //moves in the direction of this location, if within sensor radius, blows up
+        //if the enemy bot is already destroyed, the bot must resume trying to blow up the enlightment center
+        if(attackable.length != 0 && chaseCount != -1){
+            System.out.println("TRACKING");
+
+            RobotInfo closeEnemy = attackable[0];
+            MapLocation track = closeEnemy.getLocation();
+            int[] tracked = new int[2];
+            tracked[0] += track.x;
+            tracked[1] += track.y;
+
+            System.out.println("ENEMY ROBOT: " + tracked[0] + "," + tracked[1]);
+            Direction toCloseEnemy = myLoc.directionTo(track);
+
+            if(myLoc.distanceSquaredTo(track) <= actionRadiusSquared && rc.canEmpower(actionRadiusSquared)){
+                rc.empower(actionRadiusSquared);
+                System.out.println("Empowered");
+
+            } else if (rc.canMove(Pathfinding.chooseBestNextStep(rc, toCloseEnemy))) {
+                rc.move(Pathfinding.chooseBestNextStep(rc, toCloseEnemy));
+                chaseCount++;
+            }
+        }
+
+
             System.out.println("I moved!");
             int[] coords = Communication.coordDecoder(role);
             MapLocation currentLocation = rc.getLocation();
